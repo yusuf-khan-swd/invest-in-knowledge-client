b1a95674c502f92991cc63e405748a98fc628e7c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-25 22:01:37 +0600

    show name of the course in checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
+  const { courseName } = useLoaderData();
   return (
     <div>
-      <h2>This is checkout route</h2>
+      <h2>This is checkout route {courseName} </h2>
     </div>
   );
 };
