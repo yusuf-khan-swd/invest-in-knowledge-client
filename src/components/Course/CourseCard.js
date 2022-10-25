2adaffb51a7e48cf5a7a86eb712bf748f9f494c7
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-25 21:37:35 +0600

    make the full card course dynamic Link

--- src/components/Course/CourseCard.js
+++ src/components/Course/CourseCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CourseCard = ({ course }) => {
-  const { courseName, img } = course;
+  const { courseName, img, courseId } = course;
   return (
-    <div>
-      <h3>{courseName}</h3>
-      <img style={{ "width": "250px" }} src={img} alt="" />
-    </div>
+    <Link to={`/courses/${courseId}`}>
+      <div>
+        <h3>{courseName}</h3>
+        <img style={{ "width": "250px" }} src={img} alt="" />
+      </div>
+
+    </Link>
   );
 };
 
