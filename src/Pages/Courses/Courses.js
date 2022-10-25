11ba5af40ef98c6c9491262aa7a5e4d2c2133c92
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-25 21:32:28 +0600

    load courses name for category

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import CourseCard from '../../components/Course/CourseCard';
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import CourseCard from "../../components/Course/CourseCard";
 
 const Courses = () => {
   const coursesCategory = useLoaderData();
   return (
     <div>
-      {
-        coursesCategory.map(course => <CourseCard key={course.id} course={course}></CourseCard>)
-      }
+      <div>
+        {coursesCategory.map((course) => (
+          <button key={course.id}>
+            <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
+          </button>
+        ))}
+      </div>
+      <div>
+        {coursesCategory.map((course) => (
+          <CourseCard key={course.id} course={course}></CourseCard>
+        ))}
+      </div>
     </div>
   );
 };
