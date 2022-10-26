4dab0656ee9b09da4432435eba11dcafef05e379
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 16:36:13 +0600

    Course Card and side complete

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -5,20 +5,22 @@ import CourseCard from "../../components/CourseCard/CourseCard";
 const Courses = () => {
   const coursesCategory = useLoaderData();
   return (
-    <div className="grid grid-cols-5 container mx-auto pt-12 pb-20">
-      <div className="hidden md:block md:col-span-2 lg:col-span-1">
-        <div className="sticky top-0">
+    <div className="container mx-auto">
+      <div className="grid grid-cols-5 pt-12 pb-20 gap-4">
+        <div className="hidden lg:block col-span-1">
+          <div className="sticky top-1">
+            {coursesCategory.map((course) => (
+              <p title={course.courseName} className="underline mb-3" key={course.id}>
+                <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="col-span-5 lg:col-span-4">
           {coursesCategory.map((course) => (
-            <p className="underline m-2 mb-3" key={course.id}>
-              <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
-            </p>
+            <CourseCard key={course.id} course={course}></CourseCard>
           ))}
         </div>
-      </div>
-      <div className="col-span-5 md:col-span-3 lg:col-span-4">
-        {coursesCategory.map((course) => (
-          <CourseCard key={course.id} course={course}></CourseCard>
-        ))}
       </div>
     </div>
   );
--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -4,40 +4,38 @@ import { Link } from 'react-router-dom';
 const CourseCard = ({ course }) => {
   const { courseId, courseName, description, img } = course;
   return (
-    <Link to={`/courses/${courseId}`}>
-      <div className="border px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="relative mb-6 sm:mx-auto md:mb-10 md:max-w-md lg:max-w-lg">
-          <img
-            className="object-cover w-full h-56 rounded shadow-lg md:h-64 lg:h-80"
-            src={img}
-            alt=""
-          />
-          <span
-            className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"
-          >
-          </span>
-        </div>
-        <div className="mb-16 md:mb-0 md:max-w-xl sm:mx-auto md:text-center">
-          <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-            {courseName}
-          </h2>
-          <p className="mb-5 text-base text-gray-700 md:text-lg">
-            {description}
-          </p>
-          <div className="flex items-center md:justify-center">
+    <div className='mb-8'>
+      <Link className='cursor-default' to={`/courses/${courseId}`}>
+        <div className="border rounded px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="relative mb-6 sm:mx-auto md:mb-10 md:max-w-md lg:max-w-lg">
+            <img
+              className="object-cover w-full h-56 rounded shadow-lg md:h-64 lg:h-80"
+              src={img}
+              alt=""
+            />
             <span
+              className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"
             >
-              Get started
-            </span>
-            <span
-              className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Learn more
             </span>
           </div>
+          <div className="mb-16 md:mb-0 md:max-w-xl sm:mx-auto text-center">
+            <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              {courseName}
+            </h2>
+            <p className="mb-5 text-base text-gray-700 md:text-lg">
+              {description}
+            </p>
+            <div>
+              <button
+                className='inline-flex border w-full items-center justify-center py-2 px-4 font-medium transition-colors duration-200 rounded-lg text-gray-300 bg-gray-700 hover:bg-gray-600'
+              >
+                Get Details
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
