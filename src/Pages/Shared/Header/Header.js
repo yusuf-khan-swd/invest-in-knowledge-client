efdcd313b0643dcca48a7cdc0099be0b063b4479
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 10:54:49 +0600

    added a better nav for site

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
 import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const { user, logOut } = useContext(AuthContext);
 
   const handleLogOut = () => {
@@ -20,22 +22,216 @@ const Header = () => {
 
   return (
     <nav>
-      <Link to='/home'>Home</Link>
-      <Link to='/courses'>Courses</Link>
-      <Link to='/faq'>FAQ</Link>
-      <Link to='/blog'>Blog</Link>
-      {
-        user?.uid ?
-          <>
-            <span> {user.displayName} </span>
-            <button onClick={handleLogOut}>logout</button>
-          </>
-          :
-          <>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Register</Link>
-          </>
-      }
+      <div class="bg-gray-600 text-gray-400">
+        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div class="relative flex items-center justify-between">
+            <a
+              href="/"
+              aria-label="Company"
+              title="Company"
+              class="inline-flex items-center"
+            >
+              <svg
+                class="w-8 text-teal-accent-400"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                stroke="currentColor"
+                fill="none"
+              >
+                <rect x="3" y="1" width="7" height="12" />
+                <rect x="3" y="17" width="7" height="6" />
+                <rect x="14" y="1" width="7" height="6" />
+                <rect x="14" y="11" width="7" height="12" />
+              </svg>
+              <span class="ml-2 text-xl font-bold tracking-wide uppercase">
+                Invest In Knowledge
+              </span>
+            </a>
+            <ul class="items-center hidden space-x-8 lg:flex">
+              <li>
+                <Link
+                  to="/home"
+                  aria-label="Our home"
+                  title="Our home"
+                  class="font-medium tracking-wide transition-colors duration-200 hover:text-white"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/courses"
+                  aria-label="Our courses"
+                  title="Our courses"
+                  class="font-medium tracking-wide transition-colors duration-200 hover:text-white"
+                >
+                  Courses
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/faq"
+                  aria-label="faq"
+                  title="Frequently asked question"
+                  class="font-medium tracking-wide transition-colors duration-200 hover:text-white"
+                >
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/blog"
+                  aria-label="blog"
+                  title="Blog"
+                  class="font-medium tracking-wide transition-colors duration-200 hover:text-white"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide transition duration-200 rounded shadow-md bg-deep-white hover:bg-deep-white focus:shadow-outline focus:outline-none"
+                  aria-label="Sign up"
+                  title="Sign up"
+                >
+                  Sign up
+                </a>
+              </li>
+            </ul>
+            <div class="lg:hidden">
+              <button
+                aria-label="Open Menu"
+                title="Open Menu"
+                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => setIsMenuOpen(true)}
+              >
+                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                  />
+                </svg>
+              </button>
+              {isMenuOpen && (
+                <div class="absolute top-0 left-0 w-full">
+                  <div class="p-5 bg-white border rounded shadow-sm">
+                    <div class="flex items-center justify-between mb-4">
+                      <div>
+                        <a
+                          href="/"
+                          aria-label="Company"
+                          title="Company"
+                          class="inline-flex items-center"
+                        >
+                          <svg
+                            class="w-8 text-deep-white"
+                            viewBox="0 0 24 24"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            stroke="currentColor"
+                            fill="none"
+                          >
+                            <rect x="3" y="1" width="7" height="12" />
+                            <rect x="3" y="17" width="7" height="6" />
+                            <rect x="14" y="1" width="7" height="6" />
+                            <rect x="14" y="11" width="7" height="12" />
+                          </svg>
+                          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                            Company
+                          </span>
+                        </a>
+                      </div>
+                      <div>
+                        <button
+                          aria-label="Close Menu"
+                          title="Close Menu"
+                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          onClick={() => setIsMenuOpen(false)}
+                        >
+                          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                            <path
+                              fill="currentColor"
+                              d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <nav>
+                      <ul class="space-y-4">
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Our product"
+                            title="Our product"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-white"
+                          >
+                            Product
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Our product"
+                            title="Our product"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-white"
+                          >
+                            Features
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Product pricing"
+                            title="Product pricing"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-white"
+                          >
+                            Pricing
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="About us"
+                            title="About us"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-white"
+                          >
+                            About us
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            aria-label="Sign up"
+                            title="Sign up"
+                          >
+                            Sign up
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
