47f901a95f1e8be7f3cdbcd1f46699d93d8ba4ab
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 11:02:40 +0600

    added remove default name add dark and light name

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -92,14 +92,14 @@ const Header = () => {
                 </Link>
               </li>
               <li>
-                <label for="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100">
-                  <span>Left</span>
+                <label for="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-400">
+                  <span>Light</span>
                   <span className="relative">
                     <input id="Toggle1" type="checkbox" className="hidden peer" />
                     <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
                     <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
                   </span>
-                  <span>Right</span>
+                  <span>Dark</span>
                 </label>
               </li>
               <li>
