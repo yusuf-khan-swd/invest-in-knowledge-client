f60cc933d1e06eec5e6b8b2b782c9930ad765714
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 18:07:14 +0600

    format with prettier and modified some style

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaDownload } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
 
 const CourseDetails = () => {
@@ -7,12 +8,19 @@ const CourseDetails = () => {
   return (
     <div className="rounded px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="mb-16 md:mb-0 md:max-w-xl sm:mx-auto text-center">
-        <Link to="/courses">
-          <button className="border-2 border-gray-600 py-1 px-2 rounded mb-3 hover:bg-gray-300">Go Back</button>
-        </Link>
-        <h2 className="mb-8 mt-3 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-          {courseName}
-        </h2>
+        <div>
+          <Link to="/courses">
+            <button className="border-2 border-gray-600 py-1 px-2 rounded mb-3 hover:bg-gray-300">
+              Go Back
+            </button>
+          </Link>
+          <h2 className="mb-8 mt-3 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+            {courseName}
+          </h2>
+          <button className="mb-5">
+            <FaDownload className="text-2xl text-gray-500"></FaDownload>
+          </button>
+        </div>
         <div className="relative mb-6 sm:mx-auto md:mb-10 md:max-w-md lg:max-w-lg">
           <img
             className="object-cover w-full h-56 rounded shadow-lg md:h-64 lg:h-80"
@@ -21,17 +29,22 @@ const CourseDetails = () => {
           />
           <span className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"></span>
         </div>
-        <p className="mb-5 text-base text-gray-700 md:text-lg">{description}</p>
+        <p className="mb-5 text-left text-base text-gray-700 md:text-lg">
+          {description}
+        </p>
         <div className="text-left cursor-pointer mt-3 mb-5">
-          {details.map((detail) => (
-            <li className="hover:dark:text-violet-500 list-decimal mb-1">
+          {details.map((detail, index) => (
+            <li
+              key={index}
+              className="hover:dark:text-violet-500 list-decimal mb-1"
+            >
               {detail}
             </li>
           ))}
         </div>
         <div>
-          <button className="inline-flex border w-full items-center justify-center py-2 px-4 font-medium transition-colors duration-200 rounded-lg text-gray-300 bg-gray-700 hover:bg-gray-600">
-            Get Details
+          <button className="inline-flex border w-full items-center justify-center py-2 px-4 font-medium transition-colors duration-200 rounded-lg bg-amber-400 hover:bg-amber-600">
+            Get premium access
           </button>
         </div>
       </div>
