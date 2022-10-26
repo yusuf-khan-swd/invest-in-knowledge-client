113b0d0c6dc6bcf5e8e471fdcdc25aa1725d2024
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 16:16:52 +0600

    added card courseCard

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -6,14 +6,16 @@ const Courses = () => {
   const coursesCategory = useLoaderData();
   return (
     <div className="grid grid-cols-5 container mx-auto pt-12 pb-20">
-      <div className="col-span-1">
-        {coursesCategory.map((course) => (
-          <p className="underline m-2 mb-3" key={course.id}>
-            <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
-          </p>
-        ))}
+      <div className="hidden md:block md:col-span-2 lg:col-span-1">
+        <div className="sticky top-0">
+          {coursesCategory.map((course) => (
+            <p className="underline m-2 mb-3" key={course.id}>
+              <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
+            </p>
+          ))}
+        </div>
       </div>
-      <div className="col-span-4">
+      <div className="col-span-5 md:col-span-3 lg:col-span-4">
         {coursesCategory.map((course) => (
           <CourseCard key={course.id} course={course}></CourseCard>
         ))}
--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -5,10 +5,37 @@ const CourseCard = ({ course }) => {
   const { courseId, courseName, description, img } = course;
   return (
     <Link to={`/courses/${courseId}`}>
-      <div>
-        <h3>{courseName}</h3>
-        <img style={{ "width": "250px" }} src={img} alt="" />
-        <p>{description}</p>
+      <div className="border px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="relative mb-6 sm:mx-auto md:mb-10 md:max-w-md lg:max-w-lg">
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg md:h-64 lg:h-80"
+            src={img}
+            alt=""
+          />
+          <span
+            className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"
+          >
+          </span>
+        </div>
+        <div className="mb-16 md:mb-0 md:max-w-xl sm:mx-auto md:text-center">
+          <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+            {courseName}
+          </h2>
+          <p className="mb-5 text-base text-gray-700 md:text-lg">
+            {description}
+          </p>
+          <div className="flex items-center md:justify-center">
+            <span
+            >
+              Get started
+            </span>
+            <span
+              className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              Learn more
+            </span>
+          </div>
+        </div>
       </div>
     </Link>
   );
