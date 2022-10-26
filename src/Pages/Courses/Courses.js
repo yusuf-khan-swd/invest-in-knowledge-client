711f89ae2f189af24c784fa4e52f741fbcf109fc
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 15:22:49 +0600

    added grid

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -5,15 +5,15 @@ import CourseCard from "../../components/Course/CourseCard";
 const Courses = () => {
   const coursesCategory = useLoaderData();
   return (
-    <div>
-      <div>
+    <div className="grid grid-cols-5 container mx-auto">
+      <div className="col-span-1">
         {coursesCategory.map((course) => (
-          <button key={course.id}>
+          <p className="underline m-2 mb-3" key={course.id}>
             <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
-          </button>
+          </p>
         ))}
       </div>
-      <div>
+      <div className="col-span-4">
         {coursesCategory.map((course) => (
           <CourseCard key={course.id} course={course}></CourseCard>
         ))}
