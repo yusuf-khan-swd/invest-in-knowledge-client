90c8c36348103a0e4cc3227e4ae3d6d1c5d5710d
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 21:48:16 +0600

    remove toggle in menu

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -260,17 +260,6 @@ const Header = () => {
                             Blog
                           </Link>
                         </li>
-                        <li>
-                          <label htmlFor="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-400">
-                            <span>Light</span>
-                            <span className="relative">
-                              <input id="Toggle1" type="checkbox" className="hidden peer" />
-                              <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
-                              <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
-                            </span>
-                            <span>Dark</span>
-                          </label>
-                        </li>
                         {
                           user?.uid ?
                             <>
