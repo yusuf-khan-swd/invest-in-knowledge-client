33e802b1daeeb2570fa86b9119bd7a9b38d14df2
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-26 10:59:08 +0600

    added toggle btn for dark and light

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -92,6 +92,17 @@ const Header = () => {
                 </Link>
               </li>
               <li>
+                <label for="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100">
+                  <span>Left</span>
+                  <span className="relative">
+                    <input id="Toggle1" type="checkbox" className="hidden peer" />
+                    <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
+                    <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
+                  </span>
+                  <span>Right</span>
+                </label>
+              </li>
+              <li>
                 <a
                   href="/"
                   class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide transition duration-200 rounded shadow-md bg-deep-white hover:bg-deep-white focus:shadow-outline focus:outline-none"
