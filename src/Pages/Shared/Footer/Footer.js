5f2df444b09577ba69bc5aa411091c5ec28ebf66
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-27 16:30:32 +0600

    modified the footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
           <div className="flex flex-col space-y-4">
             <h2 className="font-medium">Getting started</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
-              <Link rel="noopener noreferrer" href="#">Installation</Link>
-              <Link rel="noopener noreferrer" href="#">Release Notes</Link>
-              <Link rel="noopener noreferrer" href="#">Upgrade Guide</Link>
-              <Link rel="noopener noreferrer" href="#">Using with Preprocessors</Link>
+              <Link to='/home' href="#">Home</Link>
+              <Link to='/courses' href="#">Courses</Link>
+              <Link to='/faq' href="#">FAQs</Link>
+              <Link to='/blog' href="#">Blog</Link>
               <Link rel="noopener noreferrer" href="#">Optimizing for Production</Link>
               <Link rel="noopener noreferrer" href="#">Browser Support</Link>
               <Link rel="noopener noreferrer" href="#">IntelliSense</Link>
@@ -21,7 +21,7 @@ const Footer = () => {
           <div className="flex flex-col space-y-4">
             <h2 className="font-medium">Core Concepts</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
-              <Link rel="noopener noreferrer" href="#">Utility-First</Link>
+              <Link rel="noopener noreferrer" href="#">Private Route</Link>
               <Link rel="noopener noreferrer" href="#">Responsive Design</Link>
               <Link rel="noopener noreferrer" href="#">Hover, Focus, &amp; Other States</Link>
               <Link rel="noopener noreferrer" href="#">Dark Mode</Link>
@@ -53,7 +53,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="flex items-center justify-center px-6 pt-12 text-sm">
-          <span className="dark:text-gray-400">© Copyright 1986. All Rights Reserved.</span>
+          <span className="dark:text-gray-400">© Copyright 2022. All Rights Reserved.</span>
         </div>
       </footer>
     </div>
