ef30e673d6f9fbfd0a5c73529da50b495759da5c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-27 16:15:48 +0600

    modified courses structure how it show in ui

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -6,17 +6,17 @@ const Courses = () => {
   const coursesCategory = useLoaderData();
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-5 pt-12 pb-20 gap-4">
-        <div className="hidden lg:block col-span-1">
-          <div className="sticky top-1">
+      <div className="grid grid-cols-1 lg:grid-cols-5 pt-12 pb-20 gap-4">
+        <div className="col-span-1 ml-3">
+          <div className="sticky top-2">
             {coursesCategory.map((course) => (
-              <p title={course.courseName} className="underline mb-3" key={course.id}>
+              <p title={course.courseName} className="md:underline mb-3 text-blue-900" key={course.id}>
                 <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
               </p>
             ))}
           </div>
         </div>
-        <div className="col-span-5 lg:col-span-4">
+        <div className="col-span-4">
           {coursesCategory.map((course) => (
             <CourseCard key={course.id} course={course}></CourseCard>
           ))}
