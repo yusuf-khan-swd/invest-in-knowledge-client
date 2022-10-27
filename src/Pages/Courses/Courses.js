1eafe13b8f1c295aa52312cf4174127472464b6d
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-27 18:46:56 +0600

    added dark theme to courses

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -8,24 +8,24 @@ const Courses = () => {
   const coursesCategory = useLoaderData();
   const { darkTheme } = useContext(ThemeContext);
 
-  console.log(darkTheme)
-
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-1 lg:grid-cols-5 pt-12 pb-20 gap-4 ">
-        <div className="col-span-1 ml-3">
-          <div className="sticky top-2">
+    <div className={`${darkTheme ? 'bg-gray-400' : 'bg-white'}`}>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-5 pt-12 pb-20 gap-4">
+          <div className="col-span-1 ml-3">
+            <div className="sticky top-2">
+              {coursesCategory.map((course) => (
+                <p title={course.courseName} className={`md:underline mb-3 ${darkTheme ? 'text-gray-100' : 'text-blue-900'}`} key={course.id}>
+                  <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="col-span-4">
             {coursesCategory.map((course) => (
-              <p title={course.courseName} className="md:underline mb-3 text-blue-900" key={course.id}>
-                <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
-              </p>
+              <CourseCard key={course.id} course={course}></CourseCard>
             ))}
           </div>
-        </div>
-        <div className="col-span-4">
-          {coursesCategory.map((course) => (
-            <CourseCard key={course.id} course={course}></CourseCard>
-          ))}
         </div>
       </div>
     </div>
