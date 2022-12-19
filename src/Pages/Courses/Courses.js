9a4acc954b4a0b374d363b73f13ac44b75729c14
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-19 14:00:31 +0600

    added background color to courses name

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -13,10 +13,10 @@ const Courses = () => {
       <div className="container mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-5 pt-12 pb-20 gap-4">
           <div className="col-span-1 ml-3">
-            <div className="sticky top-2">
+            <div className="sticky top-3">
               {coursesCategory.map((course) => (
-                <p title={course.courseName} className={`mb-1 font-semibold hover:underline ${darkTheme ? 'text-gray-100' : 'text-blue-900'}`} key={course.id}>
-                  <Link to={`/courses/${course.courseId}`}>{course.courseName}</Link>
+                <p title={course.courseName} className={`mb-2 rounded py-1 px-2 font-semibold ${darkTheme ? 'text-gray-100 bg-gray-500' : 'text-blue-900 bg-gray-200'}`} key={course.id}>
+                  <Link className="hover:underline " to={`/courses/${course.courseId}`}>{course.courseName}</Link>
                 </p>
               ))}
             </div>
