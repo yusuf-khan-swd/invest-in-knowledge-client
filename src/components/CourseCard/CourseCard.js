5bca8ca1c45a8c5b0f90db2512b54264daca6fb0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-19 13:37:59 +0600

    image rounded issue solve

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -12,12 +12,12 @@ const CourseCard = ({ course }) => {
         <div className="border border-slate-400 rounded px-4 sm:py-16 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-md md:px-24 lg:px-8 lg:py-20">
           <div className="relative mb-6 md:mx-auto md:mb-10 md:max-w-md lg:max-w-lg">
             <img
-              className="object-cover w-full h-56 rounded shadow-lg md:h-64 lg:h-80"
+              className="object-cover w-full h-56 rounded-3xl shadow-lg md:h-64 lg:h-80"
               src={img}
               alt=""
             />
             <span
-              className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"
+              className="absolute rounded-3xl inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"
             >
             </span>
           </div>
--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -13,54 +13,58 @@ const CourseDetails = () => {
   const ref = React.createRef();
 
   return (
-    <div className={`py-10 ${darkTheme ? 'bg-gray-700 text-gray-200' : 'bg-white'}`}>
-      <div ref={ref} className="my-10 border border-slate-400 rounded px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-md md:px-24 lg:px-8 lg:py-20">
-        <div className="mb-16 md:mb-0 md:max-w-xl sm:mx-auto text-center">
-          <div>
-            <Link to="/courses">
-              <button className="border border-gray-600 py-1 px-2 rounded mb-3 transition-colors duration-150 text-gray-100 bg-slate-500 hover:bg-slate-600">
-                Go Back
-              </button>
-            </Link>
+    <div className="m-2">
+      <div className={`py-10 ${darkTheme ? 'bg-gray-700 text-gray-200' : 'bg-white'}`}>
+        <div ref={ref} className="my-10 border border-slate-400 rounded px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-md md:px-24 lg:px-8 lg:py-20">
+          <div className="mb-16 md:mb-0 md:max-w-xl sm:mx-auto text-center">
+            <div>
+              <Link to="/courses">
+                <button className="border border-gray-600 py-1 px-2 rounded mb-3 transition-colors duration-150 text-gray-100 bg-slate-500 hover:bg-slate-600">
+                  Go Back
+                </button>
+              </Link>
+            </div>
             <h2 className={`mb-8 mt-3 font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none`}>
               {courseName}
             </h2>
-            <Pdf targetRef={ref} filename="code-example.pdf">
-              {({ toPdf }) => <button onClick={toPdf}>
-                <FaDownload className="text-2xl  mb-5"></FaDownload>
-              </button>}
-            </Pdf>
-          </div>
-          <div className="relative mb-6 sm:mx-auto md:mb-10 md:max-w-md lg:max-w-lg">
-            <img
-              className="object-cover w-full h-56 rounded shadow-lg md:h-64 lg:h-80"
-              src={img}
-              alt=""
-            />
-            <span className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"></span>
-          </div>
-          <h2 className="text-3xl font-bold mb-3">Price: <span className={`text-orange-400`}>${price}</span></h2>
-          <div className="text-left">
-            <p className="mb-5 text-base text-gray-700 md:text-lg">
-              {description}
-            </p>
-            <div className="cursor-pointer mt-3 mb-5">
-              {details.map((detail, index) => (
-                <li
-                  key={index}
-                  className={`${darkTheme ? 'hover:text-gray-300' : 'hover:text-violet-500'} list-decimal mb-1`}
-                >
-                  {detail}
-                </li>
-              ))}
+            <div>
+              <Pdf targetRef={ref} filename="code-example.pdf">
+                {({ toPdf }) => <button onClick={toPdf}>
+                  <FaDownload className="text-2xl  mb-5"></FaDownload>
+                </button>}
+              </Pdf>
             </div>
-          </div>
-          <div>
-            <Link to={`/checkout/${courseId}`}>
-              <button className={`inline-flex border w-full items-center justify-center py-2 px-4 font-medium transition-colors duration-200 rounded-lg ${darkTheme ? 'bg-gray-600 hover:bg-gray-800' : 'bg-amber-400 hover:bg-amber-600'}`}>
-                Get premium access
-              </button>
-            </Link>
+            <div className="relative mb-6 sm:mx-auto md:mb-10 md:max-w-md lg:max-w-lg ">
+              <img
+                className="object-cover rounded-3xl w-full h-56 shadow-lg md:h-64 lg:h-80"
+                src={img}
+                alt=""
+              />
+              <span className="absolute rounded-3xl inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 hover:bg-opacity-25"></span>
+            </div>
+            <h2 className="text-3xl font-bold mb-3">Price: <span className={`text-orange-400`}>${price}</span></h2>
+            <div className="text-left">
+              <p className="mb-5 text-base text-gray-700 md:text-lg">
+                {description}
+              </p>
+              <div className="cursor-pointer mt-3 mb-5">
+                {details.map((detail, index) => (
+                  <li
+                    key={index}
+                    className={`${darkTheme ? 'hover:text-gray-300' : 'hover:text-violet-500'} list-decimal mb-1`}
+                  >
+                    {detail}
+                  </li>
+                ))}
+              </div>
+            </div>
+            <div>
+              <Link to={`/checkout/${courseId}`}>
+                <button className={`inline-flex border w-full items-center justify-center py-2 px-4 font-medium transition-colors duration-200 rounded-lg ${darkTheme ? 'bg-gray-600 hover:bg-gray-800' : 'bg-amber-400 hover:bg-amber-600'}`}>
+                  Get premium access
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
