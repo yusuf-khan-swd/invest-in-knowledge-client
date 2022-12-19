48fca417a474847e6b970dcece44d09ff920c78c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-19 18:58:51 +0600

    added some style to footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
   const copyrightDate = new Date().getFullYear();
 
   return (
-    <div className='bg-slate-800 text-gray-300 dark:bg-gray-800 p-10'>
+    <div className='bg-slate-800 text-gray-300 dark:bg-gray-800'>
       <footer className="p-6 dark:text-gray-100">
         <div className="container grid grid-cols-2 mx-auto gap-x-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4">
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col space-y-4 ">
             <h2 className="font-medium">Getting started</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
               <Link className="hover:text-white" to='/home'>Home</Link>
@@ -17,7 +17,6 @@ const Footer = () => {
               <Link className="hover:text-white" to='/blog'>Blog</Link>
               <Link className="hover:text-white" to="/">Production</Link>
               <Link className="hover:text-white" to="/">Browser Support</Link>
-              <Link className="hover:text-white" to="/">IntelliSense</Link>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
@@ -27,7 +26,6 @@ const Footer = () => {
               <Link className="hover:text-white" to="/">Responsive Design</Link>
               <Link className="hover:text-white" to="/">Hover, Focus</Link>
               <Link className="hover:text-white" to="/">Dark Mode</Link>
-              <Link className="hover:text-white" to="/">Adding Base Styles</Link>
               <Link className="hover:text-white" to="/">Components</Link>
               <Link className="hover:text-white" to="/">Utilities</Link>
             </div>
@@ -39,7 +37,6 @@ const Footer = () => {
               <Link className="hover:text-white" to="/">Theme</Link>
               <Link className="hover:text-white" to="/">Breakpoints</Link>
               <Link className="hover:text-white" to="/">Colors</Link>
-              <Link className="hover:text-white" to="/">Spacing</Link>
               <Link className="hover:text-white" to="/">Variants</Link>
               <Link className="hover:text-white" to="/">Plugins</Link>
             </div>
@@ -51,14 +48,13 @@ const Footer = () => {
               <Link className="hover:text-white" to="/">Discord</Link>
               <Link className="hover:text-white" to="/">Twitter</Link>
               <Link className="hover:text-white" to="/">YouTube</Link>
-              <Link className="hover:text-white" to="/">Slack</Link>
               <Link className="hover:text-white" to="/">Facebook</Link>
               <Link className="hover:text-white" to="/">LinkedIn</Link>
             </div>
           </div>
         </div>
-        <div className="text-sm bg-gray-600 rounded-xl mt-8 px-8 py-6 text-center">
-          <p className="dark:text-gray-400">&copy; Invest In knowledge {copyrightDate}. All Rights Reserved</p>
+        <div className="text-sm bg-gray-600 rounded-lg mt-8 px-8 py-8 text-center">
+          <p className="dark:text-gray-400">&copy; Copyright {copyrightDate} - <Link to={`/home`} className='text-gray-200'>Invest In knowledge</Link>  - All Rights Reserved</p>
         </div>
       </footer>
     </div>
